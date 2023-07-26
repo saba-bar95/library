9d0b8f17762af6703edca79e941c8c7e8e74b72c
saba-bar95 <s.barbakadze95@gmail.com>
2023-07-26 20:51:12 +0400

    Add common functionalities

---
+++ README.md
@@ -0,0 +1,18 @@
+### Project: Library
+
+### This project is done as part of the [Odin course](https://www.theodinproject.com/dashboard)
+
+####
+
+### Build with
+
+- HTML
+- CSS
+- Grid Layout
+
+### [Live URL](https://saba-bar95.github.io/library/)
+
+### Author
+
+- [GitHub](https://github.com/saba-bar95)
+- [Coderbyte](https://coderbyte.com/profile/gegewp)
--- script.js
+++ script.js
@@ -1,15 +1,75 @@
+// ================= BUTTONS
 const newBookBtn = document.querySelector(".new-book-btn");
-const bookAdderDic = document.querySelector(".book-adder");
+const addBtn = document.querySelector(".add-btn");
 
-newBookBtn.addEventListener("click", function () {
-  bookAdderDic.classList.toggle("hidden");
+// ================= DIVS & INPUTS
+const bookAdderDiv = document.querySelector(".book-adder");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pagesDiv = document.querySelector(".pages-div");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+
+// ================= PARAGRAPHS
+const booksLibrary = document.querySelector(".books-library");
+const tBody = document.querySelector("tbody");
+const tr = document.createElement("tr");
+
+const myLibrary = [];
+
+function Book(title, author, read, pages) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
+  this.pages = pages;
+}
+
+function displayBook(obj) {
+  const tr = document.createElement("tr");
+  tBody.prepend(tr);
+  for (let [key, value] of Object.entries(obj)) {
+    const td = document.createElement("td");
+    if (value === "Not read") td.classList.add("not-read");
+    if (value === "0") return;
+    td.textContent = value;
+    tr.appendChild(td);
+  }
+}
+
+// ================= EVENT LISTENERS
+
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!title.validity.valid || !author.validity.valid) return;
+  if (read.value === "Read" && pages.value === "0") return;
+  if (title.validity.valid && author.validity.valid) {
+    const newBookObject = new Book(
+      title.value,
+      author.value,
+      read.value,
+      pages.value
+    );
+    myLibrary.push(newBookObject);
+    displayBook(newBookObject);
+    if (booksLibrary.classList.contains("hidden")) {
+      booksLibrary.classList.remove("hidden");
+    }
+    pages.value = "0";
+    title.value = author.value = "";
+  }
 });
 
-let myLibrary = [];
+newBookBtn.addEventListener("click", function () {
+  if (read.value === "Read") pagesDiv.classList.remove("hidden");
+  bookAdderDiv.classList.toggle("hidden");
+  pages.value = "0";
+  title.value = author.value = "";
+});
 
-function Book() {}
-
-function addBookToLibrary() {
-  myLibrary.push("dsadas");
-  myLibrary.push("sss");
-}
+read.addEventListener("change", function () {
+  if (read.value === "Read") pagesDiv.classList.remove("hidden");
+  else {
+    pagesDiv.classList.add("hidden");
+    pages.value = 0;
+  }
+});
